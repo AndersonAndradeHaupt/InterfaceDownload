3f3b9d31773599cdc09d053246ed4d58e3ed070f
andersonandradehaupt <andersonandradehaupt@gmail.com>
2024-06-18 13:28:34 -0400

    Ajustes

--- Interface/Download.interfaces.pas
+++ Interface/Download.interfaces.pas
@@ -8,7 +8,9 @@ uses
 type
   iDownload = Interface
   ['{D4E0525A-D8CB-4277-B8D0-F7A2518AC0E3}']
-  function Download(const URL, FileName: string):Boolean;
+  function Download():Boolean;
+  function link(aLink :string):iDownload;
+  function filename(aFilename :string):iDownload;
 end;
 
 implementation
