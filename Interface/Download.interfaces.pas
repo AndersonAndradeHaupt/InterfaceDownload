88f9f621ab031073edbba748995d57d65d7a7236
AndersonAndradeHaupt <80731486+AndersonAndradeHaupt@users.noreply.github.com>
2024-06-18 14:26:15 -0400

    Ajustes

--- Interface/Download.interfaces.pas
+++ Interface/Download.interfaces.pas
@@ -9,8 +9,8 @@ type
   iDownload = Interface
   ['{D4E0525A-D8CB-4277-B8D0-F7A2518AC0E3}']
   function Download():Boolean;
-  function link(aLink :string):iDownload;
-  function filename(aFilename :string):iDownload;
+  function Link(aLink :string):iDownload;
+  function Filename(aFilename :string):iDownload;
 end;
 
 implementation
